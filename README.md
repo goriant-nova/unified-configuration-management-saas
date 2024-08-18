56a6b572b2071ba0f9ce5e46ef5ee1d6bd297e00
Lucas Le <lamle@gmx.com>
2024-08-18 07:36:08 +0700

    Init UCM project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # unified-configuration-management-saas
 Unified Configuration Management SaaS
+
+# UCM is monorepo
+- UCM Backend
+- UserService
+- NotificationService
---
+++ src/test/java/com/goriant/nova/ucm/UnifiedConfigurationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goriant.nova.ucm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedConfigurationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
